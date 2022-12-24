0d514511c5a4d86b9d2f06958f514c2b95d76484
Chas Huggins <hugginsc10@gmail.com>
2022-12-24 04:16:17 -0500

    added env file

--- app/app/settings.py
+++ app/app/settings.py
@@ -24,7 +24,7 @@ environ.Env.read_env('.env')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY', 'UnsecureSecretKey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

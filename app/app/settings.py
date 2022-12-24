77c71e85b536990b1bf1f8e6a8182f01a0b26fff
Chas Huggins <hugginsc10@gmail.com>
2022-12-24 04:20:46 -0500

    added env file

--- app/app/settings.py
+++ app/app/settings.py
@@ -16,7 +16,6 @@ import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 

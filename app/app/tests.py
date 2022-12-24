fc5245fb8e666c39a9a6359bf0c55fbd2399ab44
Chas Huggins <hugginsc10@gmail.com>
2022-12-24 18:59:19 -0500

    created unit test for calc module

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+SAMPLE TESTS
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+
+class CalcTests(SimpleTestCase):
+    '''
+    Test the Calc Module.
+    '''
+    def test_add_numbers(self):
+        '''
+        Test that two numbers are added together.
+        '''
+        self.assertEqual(calc.add(3, 8), 11)

3369eecda925cbfa89fccf5e15e955b8168570b5
Chas Huggins <hugginsc10@gmail.com>
2022-12-25 23:35:51 -0500

    fixed typo in test_wait_for_db_delay unit test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -25,7 +25,7 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         '''Test waiting for database when getting OperationalError.'''
         patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 2 + [True]
+            [OperationalError] * 3 + [True]
             
         call_command("wait_for_db")
         

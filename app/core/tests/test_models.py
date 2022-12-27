4e26fb37e9e38c25abb4bfc87e30b601e2280b84
Chas Huggins <hugginsc10@gmail.com>
2022-12-26 22:07:27 -0500

    added require email address to create user test and module

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -33,3 +33,8 @@ class ModelTests(TestCase):
                 password='testpass123'
             )
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        '''Test that creating a user without an email raises a ValueError exception.'''
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')

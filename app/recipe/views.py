5dbdda9f81a787ed21923bf0010487b09e2fb78a
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 18:25:04 -0500

    created router for tagviewset

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,17 @@
 '''
 Views for Recipe APIs.
 '''
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    mixins,
+)
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag,
+)
 from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -29,3 +35,20 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         '''Create a new recipe.'''
         serializer.save(user=self.request.user)
+
+class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    '''Manage tags in the database.'''
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        '''Retrieve tags for authenticated user.'''
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def get_serializer_class(self):
+        '''Return appropriate serializer class.'''
+        if self.action == 'list':
+            return serializers.TagSerializer
+        return self.serializer_class

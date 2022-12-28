397004fc894c47f8fa4ae7e52f3f5f789faf21ba
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 18:53:52 -0500

    fixed RecipeViewSet in views recipe api module

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -16,7 +16,7 @@ from recipe import serializers
 
 class RecipeViewSet(viewsets.ModelViewSet):
     '''Manage recipes in the database.'''
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]

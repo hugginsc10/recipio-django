ff0190fe3c2b54b7776c35dbf8f5d49ce375bf96
Chas Huggins <hugginsc10@gmail.com>
2022-12-27 23:52:12 -0500

    added urls for recipe module

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+'''
+Views for Recipe APIs.
+'''
+from rest_framework import viewsets, mixins
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from core.models import Recipe
+from recipe import serializers
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    '''Manage recipes in the database.'''
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        '''Retrieve recipes for authenticated user.'''
+        return self.queryset.filter(user=self.request.user).orger_by('-id')
+    

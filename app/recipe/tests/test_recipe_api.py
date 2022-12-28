ee7d837714d41f29aca5be3d754f5e0bbe7d148e
Chas Huggins <hugginsc10@gmail.com>
2022-12-27 23:58:49 -0500

    fixed typo in get_queryset

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -16,6 +16,10 @@ from recipe.serializers import RecipeSerializer
 
 RECIPES_URL = reverse('recipe:recipe-list')
 
+def detail_url(recipe_id):
+    '''Create and return a recipe detail URL.'''
+    return reverse('recipe:recipe-detail', args=[recipe_id])
+
 def create_recipe(user, **params):
     '''Create and return a sample recipe.'''
     defaults = {
@@ -78,4 +82,13 @@ class PrivateRecipeApiTests(TestCase):
         serializer = RecipeSerializer(recipes, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_view_recipe_detail(self):
+        '''Test viewing a recipe detail.'''
+        recipe = create_recipe(user=self.user)
         
+        url = detail_url(recipe.id)
+        res = self.client.get(url)
+
+        serializer = RecipeSerializer(recipe)
+        self.assertEqual(res.data, serializer.data)

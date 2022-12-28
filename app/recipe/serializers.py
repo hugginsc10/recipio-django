eb04cf2992ed90b171993048c548abf40fa729c3
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 18:18:46 -0500

    added serializer for tags

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -27,3 +27,10 @@ class RecipeDetailSerializer(RecipeSerializer):
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
 
+class TagSerializer(serializers.ModelSerializer):
+    '''Serializer for Tag objects.'''
+    
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']

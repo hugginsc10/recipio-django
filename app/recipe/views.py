4658cba5e153ef902afa8270f018cec9a2bd289b
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 00:15:04 -0500

    fixed typo in recipe api views module

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -22,6 +22,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_serializer_class(self):
         '''Return appropriate serializer class.'''
         if self.action == 'list':
-            return serializers.RecipeDetailSerializer
+            return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        '''Create a new recipe.'''
+        serializer.save(user=self.request.user)

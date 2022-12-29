4f6c84a98b615fb04c92eb7dd3a7d5f1da8f6605
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 20:53:29 -0500

    fixed typo in views.py

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -58,7 +58,7 @@ class TagViewSet(
             return serializers.TagSerializer
         return self.serializer_class
 
-class IngredientViewSet(mixins.ListModeMixin, viewsets.GenericViewSet):
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     '''Manage ingredients in the database.'''
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()

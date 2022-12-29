17b7488b1efa9814255fca2255ba26506758d59d
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 19:38:54 -0500

    added test nested serializer for Tags in recipe api module

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,8 +5,18 @@ from rest_framework import serializers
 
 from core.models import Recipe, Tag
 
+class TagSerializer(serializers.ModelSerializer):
+    '''Serializer for Tag objects.'''
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
+
 class RecipeSerializer(serializers.ModelSerializer):
     '''Serializer for Recipe objects.'''
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = Recipe
         fields = [
@@ -15,21 +25,12 @@ class RecipeSerializer(serializers.ModelSerializer):
             'time_minutes',
             'price',
             'link',
+            'tags',
         ]
         read_only_fields = ['id']
-
-
 
 class RecipeDetailSerializer(RecipeSerializer):
     '''Serializer for recipe detail view.'''
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
-
-class TagSerializer(serializers.ModelSerializer):
-    '''Serializer for Tag objects.'''
-
-    class Meta:
-        model = Tag
-        fields = ['id', 'name']
-        read_only_fields = ['id']

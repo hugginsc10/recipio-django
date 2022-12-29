5dc02c8148ce6b24fd3a282e0284171cf15d2ada
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 19:54:20 -0500

    added tests for clearing recipe tags, assigning tag to recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -29,6 +29,19 @@ class RecipeSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ['id']
 
+    def create(self, validated_data):
+        '''Create a recipe.'''
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            recipe.tags.add(tag_obj)
+        return recipe
+    
 class RecipeDetailSerializer(RecipeSerializer):
     '''Serializer for recipe detail view.'''
 

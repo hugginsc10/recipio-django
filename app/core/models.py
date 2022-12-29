828d8d4565257f8d17beca33e69b83233c2b42d8
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 20:33:21 -0500

    added ingredient model and base unit test

--- app/core/models.py
+++ app/core/models.py
@@ -54,13 +54,25 @@ class Recipe(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
-    
+    ingredients = models.ManyToManyField('Ingredient')
+
     def __str__(self):
         '''Allows us to see the title in Django Admin.'''
         return self.title
 
 class Tag(models.Model):
     '''Tag for filtering recipes.'''
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    '''Ingredient for recipes.'''
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,

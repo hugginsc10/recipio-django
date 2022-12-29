291504cefb9e7d24928703c56f63c642b48adf92
Chas Huggins <hugginsc10@gmail.com>
2022-12-28 19:20:40 -0500

    added DestroyModelMixin to TagViewSet

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -37,7 +37,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 class TagViewSet(
-    mixins.UpdateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet):
     '''Manage tags in the database.'''
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()

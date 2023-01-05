cb4a013bc44421eecc757e2039cd51c8ff9bcfc2
Chas Huggins <hugginsc10@gmail.com>
2023-01-04 22:47:56 -0500

    added tests for filtering by ingredient and tag

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,6 +1,13 @@
 '''
 Views for Recipe APIs.
 '''
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
 from rest_framework import (
     viewsets,
     mixins,
@@ -25,9 +32,23 @@ class RecipeViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
+    def _params_to_ints(self, qs):
+        '''Convert a list of string IDs to a list of integers.'''
+        return [int(str_id) for str_id in qs.split(',')]
+
     def get_queryset(self):
         '''Retrieve recipes for authenticated user.'''
-        return self.queryset.filter(user=self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset = self.queryset
+        if tags:
+            tag_ids = self._params_to_ints(tags)
+            queryset = queryset.filter(tags__id__in=tag_ids)
+        if ingredients:
+            ingredient_ids = self._params_to_ints(ingredients)
+            queryset = queryset.filter(ingredients__id__in=ingredient_ids)
+
+        return queryset.filter(user=self.request.user).order_by('id').distinct()
 
     def get_serializer_class(self):
         '''Return appropriate serializer class.'''
@@ -60,7 +81,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST,
         )
-        
+
 class BaseRecipeAttrViewSet(mixins.UpdateModelMixin,
                             mixins.DestroyModelMixin,
                             mixins.ListModelMixin,

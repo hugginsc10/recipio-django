261f386f231378b6475c2cfb1c928df3a8d37a5e
Chas Huggins <hugginsc10@gmail.com>
2023-01-18 00:38:32 -0500

    fixed settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get("DB_HOST"),
+        'HOST': 'db',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASS")
